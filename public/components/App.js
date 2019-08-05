0464c9d7366f94cf09df04ad7c6406ab2c0a1e6c
eadehemingway <eade.hemingway@gmail.com>
2019-08-05 20:09:40 +0300

    update app.js file

--- public/components/App.js
+++ public/components/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import * as d3 from 'd3';
 
-export const App = () => (
-  <section>
-    <div id="chart" />
-    <div className="button-container">
-      <button id="increase" className="increase-btn">
-        +
-      </button>
-      <button id="decrease" className="decrease-btn">
-        -
-      </button>
-    </div>
-  </section>
-);
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+ 
+  render() {
+    return (
+      <section>
+        <div id="chart" />
+        <div className="button-container">
+          <button
+            id="increase"
+            className="increase-btn"
+          >
+            +
+          </button>
+          <button
+            id="decrease"
+            className="decrease-btn"
+          >
+            -
+          </button>
+        </div>
+      </section>
+    );
+  }
+}
